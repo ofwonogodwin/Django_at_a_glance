a6c09f8aeb233ce4c4fd932e6780f8f9c16018ca
Godwin Ofwono <godwinofwono933@gmail.com>
2025-06-12 11:55:05 +0300

    Fixed all mappings of urls

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from .models import Article
 
 # Create your views here.
+
+def year_archive(request, year):
+    a_list = Article.objects.filter(pub_date__year=year)
+    context = {"year": year, "article_list": a_list}
+    return render(request, "news.html", context)
+
+def home_redirect(request):
+    return redirect('year-archive', year=2025)  # or any default year
